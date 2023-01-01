2b62c0e1f8bb130195a0b06b685a6667d0abe665
zhangjianshe <zhangjianshe@gmail.com>
2023-01-01 20:02:28 +0800

    add some annotation for code

--- src/main/java/cn/mapway/ib/ServerMain.java
+++ src/main/java/cn/mapway/ib/ServerMain.java
@@ -5,13 +5,27 @@ import org.nutz.mvc.annotation.Ok;
 
 /**
  * Server Map entry point
+ * @Modules - 声明应用的所有子模块 @Modules(scanPackage = true) //1.r.58开始默认就是true
+ * @IocBy - 设置应用所采用的 Ioc 容器
+ * @SetupBy - 应用启动以及关闭时的额外处理
+ * @Views - 自定义的扩展视图
+ * @Localization - 应用的本地化字符串设定
+ * - 自定义Session实现
+ * 所有入口函数上支持的注解
+ * 声明在主模块模块的注解，将作为所有入口函数的默认配置
+ * 在子模块中的定义更为优先
  * @author zhangjianshe@gmail.com
  */
 public class ServerMain {
 
+    /**
+     * 关于 模式匹配和配置参考
+     * http://www.nutzam.com/core/mvc/url_mapping.html
+     * @return
+     */
     @At(value = {"/index", "/home", "/"}, methods = "GET")
     @Ok("jsp:jsp.index")
     public String home() {
-        return "Hello Nutz";
+        return "geo cache @mapway.cn";
     }
 }
--- src/main/webapp/WEB-INF/jsp/index.jsp
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -1,6 +1,5 @@
 <html>
 <body>
-<h2>Hello World!</h2>
-<h1><%=request.getAttribute("obj")%></h1>
+    <h1><%=request.getAttribute("obj")%></h1>
 </body>
 </html>
